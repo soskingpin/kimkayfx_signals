262d96d920c4cd8211655af8d5259911e11d8147
soskingpin <soskingpin1@gmail.com>
2026-04-18 15:43:45 +0200

    Add settings.gradle.kts with pluginManagement repositories, dependency resolution management, and rootProject configuration for Android V2 embedding

---
+++ android/settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject {
+    // Configuration for Android V2 embedding
+    // Additional project settings can be added here
+}
